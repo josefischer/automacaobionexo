7682ceebe428e4d80a0d6f821807010207a47745
josefischer <jose.severo@spread.com.br>
2021-09-21 21:53:38 -0300

    inclusao tests

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -7,12 +7,21 @@ universidade = "Faculdade Mackenzie"
 profissao = "Analista de Sistemas"
 genero = "Masculino"
 idade = 30
+password = "pasword123"
+text = "Potenti fringilla elementum sed rhoncus diam egestas porta at ante gravida,
+         etiam platea aliquam vulputate nam curabitur rutrum tortor a quam, morbi senectus
+         metus dictum dolor orci donec pharetra at. tempus est eu nec sed turpis risus feugiat,
+         laoreet faucibus vel mattis fermentum netus massa quis, purus lacus faucibus rhoncus 
+         velit proin. cras velit euismod nostra nullam urna cursus metus ipsum eget mattis,
+          at malesuada donec velit condimentum in et etiam nam. ultrices lobortis eget lorem 
+          nibh vivamus fusce vitae ad etiam, per magna consequat sed netus eget cursus quis 
+          tellus sodales, platea feugiat nullam eget arcu ornare dictum cras. "
 
 
-describe "Automação Bionexo" do
+describe "Automação Bionexo", :all_tests do
 
     it "Preencher formulario", :new_user do
-        puts "acessando a pagina"
+        puts "acessando a pagina de Formulario"
         visit "/users/new"
         puts page.title
         expect(page.title).to eql "Automação com Batista"        
@@ -26,6 +35,21 @@ describe "Automação Bionexo" do
         fill_in 'user_age', with: idade        
         click_button 'commit'        
         expect(find('#notice').visible?).to be true
+    end
+
+    it "Inputs e textos", :input_fields do
+        puts "acessando a pagina de INputs e Text fields"
+        visit "/buscaelementos/inputsetextfield"
+        puts page.title
+        expect(page.title).to eql "Automação com Batista" 
+        fill_in 'first_name', with: name
+        fill_in 'last_name', with: last_name
+        fill_in 'password', with: password
+        fill_in 'email', with: email
+        fill_in 'textarea1', with: text
+        btnvoltar = find(:xpath, '/html/body/div[2]/div[2]/div[2]/form/div[6]/div/a')
+        btnvoltar.click
+        #binding.pry
     end
 end
 
